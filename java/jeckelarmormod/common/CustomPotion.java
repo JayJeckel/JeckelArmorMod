b745c1530d22cc2b0d367c1045f7a9d7712f4ce9
JayJeckel <jeckelmail@gmail.com>
2015-05-25 19:35:33 -0500

    Custom potion icon and clean up

--- java/jeckelarmormod/common/CustomPotion.java
+++ java/jeckelarmormod/common/CustomPotion.java
@@ -1,6 +1,10 @@
 package jeckelarmormod.common;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.potion.Potion;
+import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class CustomPotion extends Potion
 {
@@ -8,18 +12,28 @@ public class CustomPotion extends Potion
 	{
 		super(id, debuff, color);
 		this.setPotionName(name);
-		this.setIconIndex(1, 0);
 	}
 
-	@Override public boolean isReady(final int duration, final int amplifier)
+	private ResourceLocation _texture = null;
+
+	public CustomPotion setResourceLocation(final String modId, final String textureName)
 	{
-		return duration >= 1;
+		this._texture = new ResourceLocation(modId, "textures/potions/" + textureName + ".png");
+		return this;
 	}
 
-	/*@SideOnly(Side.CLIENT)
+	@Override public boolean isReady(final int duration, final int amplifier) { return duration >= 1; }
+
+	@Override public CustomPotion setIconIndex(final int col, final int row)
+	{
+		super.setIconIndex(col, row);
+		return this;
+	}
+
+	@SideOnly(Side.CLIENT)
 	@Override public int getStatusIconIndex()
 	{
-		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("<modid>:textures/potions/<filename>.png"));
-		return 1;
-	}*/
+		if (this._texture != null) { Minecraft.getMinecraft().renderEngine.bindTexture(this._texture); }
+		return super.getStatusIconIndex();
+	}
 }
