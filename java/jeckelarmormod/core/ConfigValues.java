75131c58b6c3dc6624ebb1cf1e6fbcdd456d72d7
JayJeckel <jeckelmail@gmail.com>
2015-06-08 21:37:21 -0500

    Renamed some config settings and text cleanup

--- java/jeckelarmormod/core/ConfigValues.java
+++ java/jeckelarmormod/core/ConfigValues.java
@@ -11,8 +11,9 @@ public class ConfigValues extends ConfigHandlerValues
 	{
 		this.add(this._updateChecking);
 		this.add(this._villagerTrades);
-		this.add(this._woodArmorEncumbrance);
-		this.add(this._rockArmorEncumbrance);
+		this.add(this._armorEncumbranceWoodEnable);
+		this.add(this._armorEncumbranceRockEnable);
+		this.add(this._armorEncumbranceValue);
 	}
 
 	public boolean isUpdateCheckingEnabled() { return this._updateChecking.getValue(); }
@@ -22,22 +23,22 @@ public class ConfigValues extends ConfigHandlerValues
 
 	public boolean isVillagerTradesEnabled() { return this._villagerTrades.getValue(); }
 	protected final ConfigValueBoolean _villagerTrades = new ConfigValueBoolean("EnableVillagerTrades", Configuration.CATEGORY_GENERAL,
-			"Allow the mod armors to be included in villager trades.",
+			"Allow the mod armors to be included in villager trades.\n!!IMPORTANT!! Changes to this setting require a restart before they will take effect.",
 			true);
 
-	public boolean isWoodArmorEncumbranceEnabled() { return this._woodArmorEncumbrance.getValue(); }
-	protected final ConfigValueBoolean _woodArmorEncumbrance = new ConfigValueBoolean("ArmorEncumbranceWoodEnable", Configuration.CATEGORY_GENERAL,
+	public boolean isWoodArmorEncumbranceEnabled() { return this._armorEncumbranceWoodEnable.getValue(); }
+	protected final ConfigValueBoolean _armorEncumbranceWoodEnable = new ConfigValueBoolean("ArmorEncumbranceEnableWood", Configuration.CATEGORY_GENERAL,
 			"When enabled, movement speed will be decreased while wearing wood armor.",
 			false);
 
-	public boolean isRockArmorEncumbranceEnabled() { return this._rockArmorEncumbrance.getValue(); }
-	protected final ConfigValueBoolean _rockArmorEncumbrance = new ConfigValueBoolean("ArmorEncumbranceRockEnable", Configuration.CATEGORY_GENERAL,
+	public boolean isRockArmorEncumbranceEnabled() { return this._armorEncumbranceRockEnable.getValue(); }
+	protected final ConfigValueBoolean _armorEncumbranceRockEnable = new ConfigValueBoolean("ArmorEncumbranceEnableRock", Configuration.CATEGORY_GENERAL,
 			"When enabled, movement speed will be decreased while wearing rock armor.",
 			false);
 
 	public float getArmorEncumbranceValue() { return this._armorEncumbranceValue.getValue(); }
 	protected final ConfigValueFloat _armorEncumbranceValue = new ConfigValueFloat("ArmorEncumbranceValue", Configuration.CATEGORY_GENERAL,
-			"This value sets the percentage that each level of the Armor Encumbrance enchantment reduces player movement speed by.", 0.05f, 0.0f, 1.0f);
+			"This value sets the percentage that each level of the Armor Encumbrance enchantment reduces player movement speed by.\n!!IMPORTANT!! Changes to this setting require a restart before they will take effect.", 0.05f, 0.0f, 1.0f);
 
 	@Override public void update(final Configuration config)
 	{
